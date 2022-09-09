8fce55a5bf03e5786ff22d4b7fa23cb8e1220f62
Asgertor <Asge1114@stud.kea.dk>
2022-09-09 09:59:49 +0200

    Added greet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         SuperGreeter superGreeter = new SuperGreeter();
-        superGreeter.start();
+        //superGreeter.start();
+
+        superGreeter.greet();
     }
 }
--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start(){
         System.out.println("Det virker!");
+    }
+    public void greet(){
+        Scanner sc = new Scanner(System.in);
+
+        String fh = sc.next();
+
+        System.out.println(fh);
     }
 
 }
